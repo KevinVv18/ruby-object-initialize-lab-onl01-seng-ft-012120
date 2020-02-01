275eb52d0a379a2b3b8084f329155906541ebdf1
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-01 09:46:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
     def initialize(dogs_name)
-    @namee = dogs_name
+    @name = dogs_name
   end
   def name=(dogs_name)
-    @namee = dog_name
+    @name = dog_name
   end
   def name
-    @namee
+    @name
   end
 end
