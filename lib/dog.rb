cad24f615374aecace5b7d98b6fc020a1561c56d
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-01 09:37:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,17 +11,3 @@ class Dog
     @name
   end
 end
-
-
-
-class Person
-  def initialize(name)
-    @name = name
-  end
-  def name=(name)
-    @name = name
-  end
-  def name
-    @name
-  end
-end
